d096e5f6747b4fed7753398d5bc4cf83cbd93c4b
Nakayiza Kevina <kevinanakayiza@gmail.com>
2026-03-15 08:39:07 +0300

    Made API request

---
+++ Backend/src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+app.use(express.json())
+
+//routes import
+import userRouter from '../routes/user.route.js'
+
+//routes declaration
+app.use("api/v1/users", userRouter);
+
+
+//example route: https://localhost:4000/api/v1/users/register
+
+export default app;
